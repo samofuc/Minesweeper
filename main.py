f8aa2117714bf16279f578893476bc7c2309606e
samof <samo.fuc@gmail.com>
2020-08-31 20:52:04 +0200

    Prenesel vrstico in stolpec, na katerega smo kliknili v python.

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ board = Board.Board(5,5,9)
 def osnovna_stran():
     return bottle.template('home.html', board=board)
 
+@bottle.post('/open/')
+def open_cell():
+    col = bottle.request.forms['selected_col']
+    row = bottle.request.forms['selected_row']
+    print("Samo bi rad sel spat: ")
+    print(row)
+    print(col)
+    #board.open(col, row)
+    #bottle.redirect('/')
+
 #@bottle.get('/izberi_velikost_in_zahtevnost/')
 #def naslednja_stran():
     #velikost = bottle.request.query['velikost']
