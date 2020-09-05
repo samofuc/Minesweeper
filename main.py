b7bf96f9ec0ddacf45a92c4a89772ecd645f7f35
samof <samo.fuc@gmail.com>
2020-09-05 11:43:44 +0200

    Dodal CSS in originalen izgled slik igre minesweeper.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import bottle
 import Board
+import os, sys
 
-board = Board.Board(15,15,70)
+board = Board.Board(15,15,40)
+
+app_dir = os.path.dirname(sys.argv[0])
+
+@bottle.route('/<filename:re:.*\.css>')
+def send_css(filename):
+    return bottle.static_file(filename, root=app_dir)
+
+@bottle.route('/<filename:re:.*\.gif>')
+def send_image(filename):
+    return bottle.static_file(filename, root=app_dir, mimetype='image/png')
 
 @bottle.get('/')
 def osnovna_stran():
