80a7ec84a2b7adc34a67973d0a569d10777c022b
samof <samo.fuc@gmail.com>
2020-09-22 15:21:52 +0200

    Poskrbel, da lahko igra več igralcev.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import bottle
 import Board
 import os, sys
+import uuid
 
-board = Board.Board(9,9,5)
+boards = {}
 app_dir = os.path.dirname(sys.argv[0])
 
 @bottle.route('/<filename:re:.*\.css>')
@@ -13,12 +14,22 @@ def send_css(filename):
 def send_image(filename):
     return bottle.static_file(filename, root=app_dir, mimetype='image/png')
 
+def get_board():
+    id = bottle.request.get_cookie('Minesweeper_player')
+    if (id == None or boards.get(id) == None):
+        id = str(uuid.uuid1())
+        bottle.response.set_cookie('Minesweeper_player', id, path='/')
+        boards[id] = Board.Board(9,9,5)
+
+    return boards[id]
+
 @bottle.get('/')
 def osnovna_stran():
-    return bottle.template('home.html', board=board)
+    return bottle.template('home.html', board=get_board())
 
 @bottle.post('/new/')
 def new_game():
+    board = get_board()
     board.init(board.get_width(), board.get_height(), board.get_mine_count())
     bottle.redirect('/')
 
@@ -28,6 +39,7 @@ def open_cell():
     row = int(bottle.request.forms['selected_row'])
     mouse_button = bottle.request.forms['mouse_button']
 
+    board = get_board()
     if (mouse_button == "left"):
         board.open(row, col)
     elif (mouse_button == "middle"):
